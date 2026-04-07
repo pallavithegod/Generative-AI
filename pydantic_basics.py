90a527664d142881c882d5dbba9c2d2ca3860dd8
pallavithegod <jainpallavi.delhi@gmail.com>
2026-04-08 00:21:50 +0530

    pydantic basics

--- pydantic_basics.py
+++ pydantic_basics.py
@@ -1,16 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Dict, Optional
 
 class Prod(BaseModel):
     pid : int
-    price:float = 99
+    name: str
+    features: List[str]     #typing[pydantic] both used
     in_stock:bool = True
-    name:str
+    cust_name: str = Field(
+        ...,         # required field
+        max_length=15,
+        examples="Pallavi Jain"
+    )
+    image_url: Optional[str] = None    # this field may/ may not be used 
 
-dict1 = {'pid' : '123', 'price' : 45.8, 'in_stock' : False, 'name' : "keyboard"}
-obj1 = Prod(**dict1)       #spread
+dict1 = {'pid' : '123', 'features' : ['black', 'good touch sense'], 'in_stock' : False, 'name' : "keyboard"}
+obj1 = Prod(**dict1)       #spread - needs to paas each entity seperately
 print(dict1)
 
-obj2 = Prod(pid = 123, name = "mouse",price = 45.8)
+obj2 = Prod(pid = 123, name = "mouse",features = ['good scroll', 'sleek'])
 print(obj2)
+
 
 
